becc2abe8c5753c6cd5bda6902a53f6bf0d456bb
angel cerrajero <cerra.lega@gmail.com>
2019-07-13 16:06:04 +0200

    Funcionando la comprobacion de tener un Poker en la mano

--- kata3/mano.mjs
+++ kata3/mano.mjs
@@ -2,19 +2,16 @@ let valor = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
 let palo = ["S", "H", "C", "D"];
 let mano1 = [];
 let mano2 = [];
-//la variable cartas tiene toda la baraja con sus posibles cruces.
+
 
 export const cartas2 = function barajaCartas2() {
     let baraja =[];
     for( let v=0; v<valor.length; v++){
-        //console.log(palo[p]);
         for(let p=0; p<palo.length; p++){
-          //  console.log(valor[v]);
             baraja.push(valor[v]+palo[p]);
         }
     }
     return baraja;
-    //console.log(baraja);
 }
 
 
@@ -22,14 +19,11 @@ export const cartas2 = function barajaCartas2() {
 export const cartas = function barajaCartas() {
     let baraja =[];
     for( let p=0; p<palo.length; p++){
-        //console.log(palo[p]);
         for(let v=0; v<valor.length; v++){
-          //  console.log(valor[v]);
             baraja.push(valor[v]+palo[p]);
         }
     }
     return baraja;
-    //console.log(baraja);
 }
 
 
@@ -41,13 +35,9 @@ function jugador (nombre, cartas){
         for(let c = 0; c < 5; c++){
             random = Math.floor(Math.random() * cartas.length)
             miMano[c] = cartas[random];
-            
-
         }
-
         return miMano;
     }
-
 }
 
 
@@ -86,25 +76,13 @@ function personaCartas (nombre, string) {
         for(let h=0; h < array.length; h++){
             if (barajaDeCartas[number] === array[h]){
                 arrayOrdenado.push(array[h]);
-                
-                /*
-                if (a >= b) {
-                    arrayOrdenado.push(array[number])
-                }else {
-                    arrayOrdenado.push(array[number+1])
-                }*/
-                
-                
-
             }
         }
         
     }
     for (let j=0; j<arrayOrdenado.length; j++){
         arrValor.push(arrayOrdenado[j].charAt(0));
-        
-        arrPalo.push(arrayOrdenado[j].charAt(1));
-        
+        arrPalo.push(arrayOrdenado[j].charAt(1));       
     }
     this.valor = arrValor;
     this.palo = arrPalo;
@@ -149,11 +127,43 @@ function escaleraColor (arrayValores, arrayPalos, mano) {
 }
 
 
+function poker (arrayValores, arrayPalos){
+    let validado =0;
 
-export const jugador1 = new jugador ("jugador1", cartas());
-export const jugador2 = new jugador ("jugador2", cartas());
+    for(let r = 0; r < arrayValores.length; r++){
+        if (arrayValores[r] === "T"){
+            arrayValores[r] = 10;
+        }else if (arrayValores[r] === "J"){
+            arrayValores[r] = 11;
+        }else if (arrayValores[r] === "Q"){
+            arrayValores[r] = 12;
+        }else if (arrayValores[r] === "K"){
+            arrayValores[r] = 13;
+        }else if (arrayValores[r] === "A"){
+            arrayValores[r] = 14;
+        }else{        
+            arrayValores[r] = parseInt(arrayValores[r]);
+        }
+    }
 
-const persona1 = new personaCartas ("persona1", "AS QS JS TS 9S");
+    for (let i=0; i < arrayValores.length; i++){
+        if(arrayValores[i] === arrayValores[i+1]){
+            validado++
+        }
+    }
+
+    if(validado === 3){
+     return true;
+    }
+
+}
+
+
+
+//export const jugador1 = new jugador ("jugador1", cartas());
+//export const jugador2 = new jugador ("jugador2", cartas());
+
+const persona1 = new personaCartas ("persona1", "AH AS 4C AD AS");
 const persona2 = new personaCartas ("persona2", "2C 3H 9S 8C AH");
 
 
@@ -161,7 +171,7 @@ const persona2 = new personaCartas ("persona2", "2C 3H 9S 8C AH");
 mano1 = persona1.mano;
 mano2 = persona2.mano;
 
-if(comprobar(mano1) === 1 /*&& comprobar(mano2) ===1*/){
+if(comprobar(mano1) === 1 && comprobar(mano2) ===1){
     /*console.log("Mano Correcta");
     console.log(persona1.nombre, persona1.mano);
     console.log("El array de valores ordenados es: ", persona1.valor);
@@ -171,11 +181,14 @@ if(comprobar(mano1) === 1 /*&& comprobar(mano2) ===1*/){
     console.log("El array de valores es: ", persona2.valor);
     console.log("El array de palos es: ", persona2.palo);*/
 
-    if (escaleraColor(persona1.valor, persona1.palo, persona1.manoOrdenada) === true){
-        console.log("escalera de color");
+    /*if (escaleraColor(persona1.valor, persona1.palo, persona1.manoOrdenada) === true){
+        console.log(persona1.nombre, "tiene una escalera de color");
     }
 
-
+    if (poker(persona1.valor, persona1.palo, persona1.manoOrdenada) === true){
+        console.log(persona1.nombre, "tiene un poker");
+    }*/
+    
 
 
 
